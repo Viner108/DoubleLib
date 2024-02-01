cd762b1bc7d6e95124d706374c0f812aa8217931
Viner108 <andrukovichl@mail.ru>
2024-02-01 17:35:38 +0300

    Initial commit

---
+++ src/main/java/org/example/Army.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import com.lib.Unit;
+
+public class Army {
+
+    private Unit unit;
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
+}
